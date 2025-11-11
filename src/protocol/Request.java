8b4201241e8414861678067e187484b86c50a299
Gayani2002 <gunawardhanagwgm.22@uom.lk>
2025-11-11 17:12:14 +0530

    admin client

--- src/protocol/Request.java
+++ src/protocol/Request.java
@@ -6,3 +6,24 @@
  - Used together with Response.java and MessageParser.java
  - Placeholder comment only.
 */
+
+package protocol;
+
+public class Request {
+    private String action;
+    private String username;
+    private String password;
+    private int winningNumber;
+
+    public Request(String action) {
+        this.action = action;
+    }
+
+    public String getAction() { return action; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public int getWinningNumber() { return winningNumber; }
+    public void setWinningNumber(int winningNumber) { this.winningNumber = winningNumber; }
+}
